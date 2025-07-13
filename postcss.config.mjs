dcf31c4906e58eb7b670dbadcacee11e5ef593cb
hax2 <1162504@student.birzeit.edu>
2025-07-13 21:58:13 +0200

    whatever

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
+const config = {
   plugins: {
-    tailwindcss: tailwindcss(),
-    autoprefixer: autoprefixer,
-  },
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+  }
 };
+
+export default config;
