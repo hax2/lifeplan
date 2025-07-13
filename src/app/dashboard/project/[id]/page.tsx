ac4b8080c98106c78f35bd7dc9903f490e7fce0e
hax2 <1162504@student.birzeit.edu>
2025-07-13 21:09:08 +0200

    build

--- src/app/dashboard/project/[id]/page.tsx
+++ src/app/dashboard/project/[id]/page.tsx
@@ -177,7 +177,7 @@ export default function ProjectDetailPage() {
                 </div>
                 <form onSubmit={handleAddSubtask} className="flex items-center gap-3">
                     <input
-                        type="text" value={newSubtaskText} onChange={(e) => setNewSubtaskText(e.test.value)}
+                        type="text" value={newSubtaskText} onChange={(e) => setNewSubtaskText(e.target.value)}
                         placeholder="Add a new checklist item..."
                         className="w-full px-4 py-2 text-md border border-slate-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-sky-500"
                     />
